d72a11c050ac3a47460f8fd92723078a3b2e5d3c
sparkworm <imnota.squirp@gmail.com>
2026-06-14 15:37:25 -0400

    Added basic player movement.

---
+++ scenes/characters/player.gd
@@ -0,0 +1,15 @@
+class_name Player
+extends CharacterBody2D
+
+@onready var velocity_component: VelocityComponent = $VelocityComponent
+
+func _physics_process(delta: float) -> void:
+	var move_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if move_dir != Vector2.ZERO:
+		velocity_component.accelerate(delta, move_dir)
+	else:
+		velocity_component.resolve_decel(delta)
+	velocity = velocity_component.velocity
+	move_and_slide()
+	# potentially reduce velocity component's velocity if we hit a wall
+	velocity_component.velocity = velocity
