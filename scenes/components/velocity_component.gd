d39e8183dec3cef90333563f520a5dc8626dbb00
sparkworm <imnota.squirp@gmail.com>
2026-06-14 15:27:36 -0400

    Added VelocityComponent.

---
+++ scenes/components/velocity_component.gd
@@ -0,0 +1,26 @@
+## Responsible for some basic functionality regarding the manipulation of velocity
+class_name VelocityComponent
+extends Node
+
+
+## Hard cap on the magnitude of velocity
+@export var max_speed: float
+## The assumed accel of the object used in accel()
+@export var accel: float
+## A flat decceleration meant to be applied every frame
+@export var decel: float
+
+var velocity: Vector2
+
+## reduces the magnitude of the velocity linearly by decel
+func resolve_decel(delta: float) -> void:
+	var new_mag: float = max(0,velocity.length() - decel * delta)
+	velocity = velocity.normalized() * new_mag
+
+## adds direction * magnitude to the velocity.  If the magnitude of the new velocity exceeds
+## max_speed, the magnitude is set to max_speed.  magnitude will default to accel, as that is the 
+## intended behavior, but a custom magnitude can technically be specified
+func accelerate(delta: float, direction: Vector2, magnitude: float = accel) -> void:
+	velocity += direction * magnitude * delta
+	if velocity.length() > max_speed:
+		velocity = velocity.normalized() * max_speed
