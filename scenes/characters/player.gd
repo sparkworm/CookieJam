b30d475e967fa0dbf2ff25325f163e2cbd2ae01d
sparkworm <imnota.squirp@gmail.com>
2026-06-14 19:51:31 -0500

    Constructed player appearance from texture pieces.  Also made player face mouse pointer.

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 @onready var velocity_component: VelocityComponent = $VelocityComponent
 
 func _physics_process(delta: float) -> void:
+	# point towards mouse
+	look_at(get_global_mouse_position())
 	var move_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if move_dir != Vector2.ZERO:
 		velocity_component.accelerate(delta, move_dir)
