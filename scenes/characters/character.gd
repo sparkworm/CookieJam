f36e5f9ff8679e601e0fc1a0b3934ff863f376c6
sparkworm <imnota.squirp@gmail.com>
2026-06-16 20:00:26 -0500

    Added basic blood pooling.

--- scenes/characters/character.gd
+++ scenes/characters/character.gd
@@ -4,9 +4,17 @@
 class_name Character
 extends CharacterBody2D
 
+const BLOOD_POOL_DT = preload("uid://270mt62wcar")
+
+
 @export var health_component: HealthComponent
 @export var velocity_component: VelocityComponent
 @export var active_weapon: Weapon
+
+@export var blood_pools: int = 3
+@export var blood_pools_variation: int = 1
+
+var alive: bool = true
 
 func _ready() -> void:
 	health_component.health_depleted.connect(die)
@@ -21,6 +29,15 @@ func apply_velocity() -> void:
 
 ## TODO: add dead body
 func die() -> void:
+	if not alive:
+		return
+	alive = false
+	print("dying")
+	var to_spawn: int = blood_pools + randi_range(-blood_pools_variation, blood_pools_variation)
+	while to_spawn > 0:
+		var pool: DecalTransport = BLOOD_POOL_DT.instantiate()
+		MessageBus.decal_transport_spawned.emit(global_position, 0.0, pool)
+		to_spawn -= 1
 	queue_free()
 
 ## ASSUME: incoming damage is a positive value
--- scenes/effects/blood_pool.gd
+++ scenes/effects/blood_pool.gd
@@ -1,2 +1,15 @@
 class_name BloodPool
 extends Node2D
+
+## The maximum scaling of the current scale allowed
+@export var max_scale: float = 2.0
+@export var max_scale_variation: float = 0.5
+## The time spent "spreading" to reach full size
+@export var expansion_time: float = 2
+@export var expansion_time_variation: float = 1
+
+func _ready() -> void:
+	var final_scale: float = max_scale + randf_range(-1, 1) * max_scale_variation
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_property(self, "scale", Vector2(final_scale, final_scale), 
+			expansion_time + randf_range(-1, 1) * expansion_time_variation)
